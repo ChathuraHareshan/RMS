4a1eb3895d281f84e44a66da2b0c696eeba8e6b3
chathurakarunarathna <chathura.hareshan1432@gmail.com>
2026-05-13 14:56:12 +0530

    git add application.yaml git commit -m "update application.yaml" git push

--- src/main/java/com/rms/RmsApplication.java
+++ src/main/java/com/rms/RmsApplication.java
@@ -1,4 +1,18 @@
 package com.rms;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class RmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RmsApplication.class, args);
+        System.out.println("=========================================");
+        System.out.println("Restaurant Reservation Platform Started!");
+        System.out.println("Access at: http://localhost:8080/customer/login");
+        System.out.println("Access at: http://localhost:8080/admin/login");
+        System.out.println("=========================================");
+    }
 }
